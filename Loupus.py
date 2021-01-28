7c3c38def33d90f8ea1e356b9566a9ffa513143d
zhuyue199511 <2264672632@qq.com>
2021-01-28 21:01:11 +0800

    first commit

---
+++ Loupus.py
@@ -0,0 +1,4 @@
+wget https://labfile.oss.aliyuncs.com/courses/1330/linux.png
+wget https://labfile.oss.aliyuncs.com/courses/1330/python1.png
+wget https://labfile.oss.aliyuncs.com/courses/1330/python2.png
+wget https://labfile.oss.aliyuncs.com/courses/1330/git.png
